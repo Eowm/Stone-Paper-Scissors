81e1c921bbd9b8c1f2ebbfbf87d579f0fbc6adce
Adam Nelip <Adam.Nelip@gmail.com>
2018-06-07 15:15:41 +0200

    Editting basic files

--- js/index.js
+++ js/index.js
@@ -1,75 +1,133 @@
-var data = [
-	{
-		id: 'box1',
-		title: 'First box',
-		content: '<p>Lorem ipsum dolor sit amet!</p>',
-		categories: ['highlighted', 'special-header', 'important']
-	},
-	{
-		id: 'box2',
-		title: 'Second box',
-		content: '<p>Lorem ipsum dolor sit amet!</p>',
-		categories: ['special-header', 'important']
-	},
-	{
-		id: 'box3',
-		title: 'Third box',
-		content: '<p>Lorem ipsum dolor sit amet!</p>',
-		categories: ['highlighted', 'important']
-	},
-	{
-		id: 'box4',
-		title: 'Fourth box',
-		content: '<p>Lorem ipsum dolor sit amet!</p>',
-		categories: ['highlighted']
-	},
-	{
-		id: 'box5',
-		title: 'Fifth box',
-		content: '<p>Lorem ipsum dolor sit amet!</p>',
-		categories: []
-	},
-];
+'use strict';
+var output = document.getElementById('output');
+var playerResult = 0;
+var compResult = 0;
+var round = 0;
+var nrOfRounds = 0;
 
-var boxes = document.querySelectorAll('.box');
+var buttonRock = document.getElementById("rock");
+var buttonScissors = document.getElementById("scissors");
+var buttonPaper = document.getElementById("paper");
 
-var root = document.getElementById('root');
-for(var i=0; i<data.length; i++){
-    var div = '<div id="'+ data[i].id +'" class="box '+data[i].categories +'"></div>';
-    var header = '<header>'+data[i].title+'</header>';
-    var paragraf = data[i].content;
-    div.insertAdjacentHTML('beforeend', header);
-    div.insertAdjacentHTML('beforeend', paragraf);
-    root.insertAdjacentHTML('beforeend', div);
- //   document.querySelector('div[i]').classList.add(data[i].categories);
-    
-  //  var classes = boxes[i].querySelector('.box')
-  //  classes.classList.add(data[i].categories);
-    
+buttonRock.addEventListener('click', function() {
+userMove('rock') });
+buttonScissors.addEventListener('click', function() {
+userMove('scissors')});
+buttonPaper.addEventListener('click', function() { 
+userMove('paper') });
+
+var disabledButtons = function(value) {
+  buttonRock.disabled = value; 
+  buttonPaper.disabled = value; 
+  buttonScissors.disabled = value;
+};
+
+var endGame = function() {
+  playerResult = 0;
+  compResult = 0;
+  round = 0;
+  nrOfRounds = 0;
   
-    //var paragraf = 'div class="'+data[i].content+'"</div>';
-    //header.insertAdjacentHTML('beforeend', paragraf);
-}
+  output.innerHTML = ('');
+  results.innerHTML = ('');
+  rounds.innerHTML = ('');
+  roundsNr.innerHTML = ('');
+} 
 
-/*
-for(var i=0; i <data.length; i++){
-root.insertAdjacentHTML('beforeend', "<div id="+data[key][id]+" class='box'></div>");
-}
-
-var header  = document.getElementById('div');
-for(var i=0; i <data.length; i++){
-div.insertAdjacentHTML('beforeend', "<div id="+data[key][id]+" class='box'></div>");
-}
-
-
-for(var i=0; i <boxes.length ; i++){
-  var div = document.get
-  //var boxHeader = boxes[i].querySel);
-  //var boxP = boxes[i].querySelector('p');
- // var box = boxes[i].querySelector('.box')
+var newGame = function() {
+  endGame();
+  disabledButtons(false);
   
-  //box.classList.add = data[i].categories;
- // boxHeader.innerHTML = data[i].title;
- // boxP.innerHTML = data[i].content;
+  nrOfRounds = window.prompt('How many rounds would You like to play?');
+  if (isNaN(nrOfRounds) || nrOfRounds ==='' || nrOfRounds === null){
+    output.innerHTML = 'Wrong Value. Try one more time';
+    lineBreak();
+  }
+  roundsNr.innerHTML = nrOfRounds;
+  return nrOfRounds;
 }
-*/
+
+var whatCompMove = function() {
+  var possiblePicks = ['rock', 'scissors', 'paper'];
+  return possiblePicks[Math.floor(Math.random() * 3)];
+}
+  
+var userMove = function(move) {
+  var userWin = false;
+  var compWin = false;
+  var draw = false;
+  var compMove = whatCompMove();
+   switch(compMove) {
+    case 'rock' :
+      if (move == 'rock') {
+        draw = true;
+      } else if (move == 'scissors') {
+        compWin = true;
+      } else {
+        userWin = true;
+      }
+    break;
+    case 'scissors' :
+      if (move == 'rock') {
+        userWin = true;
+      } else if (move == 'scissors') {
+        draw = true;
+      } else {
+        compWin = true;
+      }
+    break;
+    case 'paper' :
+      if (move == 'rock') {
+        compWin = true;
+      } else if (move == 'scissors') {
+        userWin = true;
+      } else {
+        draw = true;
+      }
+    break;
+    }
+  result(userWin, compWin, draw, move, compMove);
+}
+  
+var result = function(userWin, compWin, draw, userMove, compMove) {
+  if (draw) {
+    output.insertAdjacentHTML('afterbegin', '</br>' + ('Its a draw'));
+    round +=1;
+  } else if (userWin) {
+    output.insertAdjacentHTML('afterbegin', '</br>' + ('YOU WON!!! with ' + userMove + ' against ' +         compMove));
+    playerResult += 1;
+    round +=1;
+  } else {
+    output.insertAdjacentHTML('afterbegin', '</br>' + ('You have lost with ' + userMove + ' against ' +       compMove));
+    compResult +=1;
+    round +=1;
+  }
+  
+  var overalResult = function() {
+    results.insertAdjacentHTML('afterbegin', '</br>' + (playerResult + " to " + compResult));
+  }
+
+  var nrRound = function() {
+    rounds.insertAdjacentHTML('afterbegin', '</br>' + (round));
+  }
+  
+  overalResult();
+  nrRound();
+  
+  if (round == nrOfRounds) {
+    if (compResult > playerResult) {
+      output.insertAdjacentHTML('afterbegin','YOU LOST! </br>');
+    } else if (compResult == playerResult) {
+        output.insertAdjacentHTML('afterbegin','ITs A DRAW </br>');
+      } else {
+        output.insertAdjacentHTML('afterbegin','YOU WON! </br>');
+      }
+    disabledButtons(true);
+  }  
+}
+
+var btnNewGame = document.getElementById("newGame");
+
+btnNewGame.addEventListener('click', function() {
+  newGame()
+} )
