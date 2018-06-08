291c7fb9257d5498f32a2f4ba218b59eb0c97904
Adam Nelip <Adam.Nelip@gmail.com>
2018-06-08 23:13:32 +0200

    Adding not working Modal 1.3

--- js/index.js
+++ js/index.js
@@ -24,6 +24,46 @@ for(var i = 0; i<moves.length; i++){
   moves[i] = userMove(element.getAttribute(data-move));
 }
 
+
+
+
+
+
+var showModal = function(event){
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.add('show');
+  };
+  var modalLinks = document.querySelectorAll('.show-modal');
+  
+  for(var i = 0; i < modalLinks.length; i++){
+    modalLinks[i].addEventListener('click', showModal);
+  }
+  var hideModal = function(event){
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.remove('show');
+  };
+  
+  var closeButtons = document.querySelectorAll('.modal .close');
+  
+  for(var i = 0; i < closeButtons.length; i++){
+    closeButtons[i].addEventListener('click', hideModal);
+  }
+  document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+  var modals = document.querySelectorAll('.modal');
+  
+  for(var i = 0; i < modals.length; i++){
+    modals[i].addEventListener('click', function(event){
+      event.stopPropagation();
+    });
+  }
+
+
+
+
+
+
+
+
 //var params = {playerResult: 0,compResult: 0,round: 0,nrOfRound: 0} 
 
 
@@ -137,7 +177,7 @@ var result = function(userWin, compWin, draw, userMove, compMove) {
   
   //sprawdzanie konca gry
   if (round == nrOfRounds) {
-    if (compResult > playerResult) {
+    if (compResult > playerResult) {//
       output.insertAdjacentHTML('afterbegin','YOU LOST! </br>');
     } else if (compResult == playerResult) {
         output.insertAdjacentHTML('afterbegin','ITs A DRAW </br>');
